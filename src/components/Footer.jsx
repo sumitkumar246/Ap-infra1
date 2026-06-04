54107232fc3d3d6e029163733eeaada5ad9cff4c
Sumit Kumar <Sumitkumar782796@gmail.com>
2026-06-04 17:48:17 +0530

    fix footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import {
   FaFacebookF,
@@ -40,12 +41,12 @@ const Footer = () => {
             <h3>Quick Links</h3>
 
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About Us</a></li>
-              <li><a href="/projects">Projects</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
+  <li><Link to="/">Home</Link></li>
+  <li><Link to="/about">About Us</Link></li>
+  <li><Link to="/projects">Projects</Link></li>
+  <li><Link to="/services">Services</Link></li>
+  <li><Link to="/contact">Contact</Link></li>
+</ul>
           </div>
 
           {/* Projects */}
