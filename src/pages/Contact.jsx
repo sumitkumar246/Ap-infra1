34b3db78f39cd034e54fbcb9ffa019722665db96
Sumit Kumar <Sumitkumar782796@gmail.com>
2026-06-05 11:55:09 +0530

    connect render backend

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      await axios.post("http://localhost:3000/api/contact", {
+      await axios.post("https://ap-infra1-1.onrender.com/api/contact", {
         ...formData,
         projectName,
         brochure,
