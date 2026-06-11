95a1c15035e09973b9b071cf5dc6855bb975a00a
Sumit Kumar <Sumitkumar782796@gmail.com>
2026-06-11 15:27:36 +0530

    add google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
+import ReactGA from "react-ga4";
 import "./index.css";
+
+ReactGA.initialize("G-PSCNQLCE2V");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
